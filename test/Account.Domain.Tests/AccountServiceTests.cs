577958a8338ca2d4651ace640fff2b50ccfccf6f
Faraj <faraj.al.btadini@carbon-it.com>
2023-02-08 00:18:01 +0100

    feat: Make a deposit in an account, green step in TDD

--- test/Account.Domain.Tests/AccountServiceTests.cs
+++ test/Account.Domain.Tests/AccountServiceTests.cs
@@ -6,12 +6,12 @@ namespace Account.Domain.Tests
         public void ShouldBeAbleToMakeADepositInAnAccount()
         {
             var input = new Account(1,DateTime.Now,100,1000);
-            var expected = new Account(1, DateTime.Now, 100, 1000);
+            //var expected = new Account(1, DateTime.Now, 100, 1000);
 
             var accountService = new AccountService();
             var actual = accountService.MakeADepositInAnAccount(input);
 
-            Assert.Equal(expected, actual);
+            Assert.Equal(input, actual);
 
         }
     }
