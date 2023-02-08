6e4c485996f4a8f82ef2dade399ac6684a3734b6
Faraj <faraj.al.btadini@carbon-it.com>
2023-02-08 01:20:13 +0100

    feat: configure account context

---
+++ src/Account.Infrastructure/AccountContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account.Infrastructure
+{
+    public sealed class AccountContext: DbContext
+    {
+        public AccountContext() 
+        {
+        }
+
+        public AccountContext(DbContextOptions<AccountContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<AccountData> AccountSet { get; set; } = default!;
+        public DbSet<OperationData> OperationSet { get; set; } = default!;
+
+    }
+}
---
+++ src/Account.Infrastructure/OperationData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account.Infrastructure
+{
+    public record class OperationData
+    {
+        public OperationData(int id, string? type, AccountData? accountData)
+        {
+            Id = id;
+            Type = type;
+            AccountData = accountData;
+        }
+
+        public int Id { get; set; }
+        public string? Type { get; set; }
+        public AccountData? AccountData { get; set; }
+    }
+}
